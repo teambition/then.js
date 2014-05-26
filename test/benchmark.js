e7709a281f2ccb12fc04b255edee5ce4e9724580
zensh <admin@zensh.com>
2014-05-27 00:00:32 +0800

    添加原生 Promise 测试对比（巨慢）
    Signed-off-by: zensh <admin@zensh.com>

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,19 +1,19 @@
 'use strict';
-/*global console*/
+/*global console, Promise*/
 
 var async = require('async'),
   thenjs = require('../then.js'),
-  Benchmark = require('jsbench');
+  JSBench = require('jsbench');
 
 var list = [], tasks = [];
 
 function task(callback) {
   // 模拟同步任务
-  // callback(null, 1);
+  callback(null, 1);
   // 模拟异步任务
-  thenjs.nextTick(function () {
-    callback(null, 1);
-  });
+  // thenjs.nextTick(function () {
+  //   callback(null, 1);
+  // });
 }
 
 // 构造任务流
@@ -22,37 +22,78 @@ for (var i = 0; i < 1000; i++) {
   tasks[i] = task;
 }
 
-var bench = new Benchmark();
+var jsbench = new JSBench();
 
-bench.add('async', function (callback) {
+if (typeof Promise === 'function') {
+  jsbench.add('Promise', function (callback) {
+    // 原生 Promise 测试主体
+    Promise.all(list.map(function (i) { // 并行 list 队列
+      return new Promise(function (resolve) {
+        task(function (error, value) {
+          resolve(value);
+        });
+      });
+    })).then(function () {
+      return list.reduce(function (promise, i) { // 串行 list 队列
+        return promise.then(function () {
+          return new Promise(function (resolve) {
+            task(function (error, value) {
+              resolve(value);
+            });
+          });
+        });
+      }, Promise.resolve());
+    }).then(function () {
+      return Promise.all(tasks.map(function (subTask) { // 并行 tasks 队列
+        return new Promise(function (resolve) {
+          subTask(function (error, value) {
+            resolve(value);
+          });
+        });
+      }));
+    }).then(function () {
+      return tasks.reduce(function (promise, subTask) { // 串行 tasks 队列
+        return promise.then(function () {
+          return new Promise(function (resolve) {
+            subTask(function (error, value) {
+              resolve(value);
+            });
+          });
+        });
+      }, Promise.resolve());
+    }).then(function () {
+      callback();
+    });
+  });
+}
+
+jsbench.add('async', function (callback) {
   // async 测试主体
-  async.each(list, function (i, next) {
+  async.each(list, function (i, next) { // 并行 list 队列
     task(next);
   }, function (err) {
     if (err) return callback(err);
-    async.eachSeries(list, function (i, next) {
+    async.eachSeries(list, function (i, next) { // 串行 list 队列
       task(next);
     }, function (err) {
       if (err) return callback(err);
-      async.series(tasks, function (err) {
+      async.parallel(tasks, function (err) { // 并行 tasks 队列
         if (err) return callback(err);
-        async.parallel(tasks, callback);
+        async.series(tasks, callback); // 串行 tasks 队列
       });
     });
   });
 }).add('thenjs', function (callback) {
   // thenjs 测试主体
-  thenjs.each(list, function (cont, i) {
+  thenjs.each(list, function (cont, i) { // 并行 list 队列
     task(cont);
   })
-  .eachSeries(list, function (cont, i) {
+  .eachSeries(list, function (cont, i) { // 串行 list 队列
     task(cont);
   })
-  .series(tasks)
-  .parallel(tasks)
-  .then(function (cont, result) {
-    callback(null, result);
-  }).fail(function (cont, error) {
+  .parallel(tasks) // 并行 tasks 队列
+  .series(tasks) // 串行 tasks 队列
+  .all(function (cont, error) {
     callback(error);
   });
 }).run(100); // 循环测试 100 次
